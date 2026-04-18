5689e060eee57ca0528404204fea3b3aa8b9e9e0
henrynnamani <hoyx0101@gmail.com>
2026-04-18 12:24:17 +0100

    app init

---
+++ index.py
@@ -0,0 +1,44 @@
+MAXIMUM_STACK_SIZE = 1024;
+
+class Stack:
+    def __init__(self):
+        self.items = [];
+
+    def __str__(self):
+        ws = []
+        for index, item in enumerate(self.items[::-1]):
+            if   index == 0                : ws.append(f"{item} <first")
+            elif index == len(self.items)-1: ws.append(f"{item} <last") 
+            else                       : ws.append(str(item))
+
+        return "\n".join(ws)
+        
+    def push(self, value):
+        if len(self.items) == MAXIMUM_STACK_SIZE - 1: raise Exception("Stack Overflow")
+        self.items.append(value)
+
+
+    def pop(self):
+        if len(self.items) == 0: raise Exception("Stack Underflow")
+        return self.items.pop()
+    
+    @property
+    def stack(self):
+        return self.items.copy()
+    
+class SimpleMemory:
+    def __init__(self):
+        self.memory = []
+
+    def access(self, offset, size):
+        return self.memory[offset:offset+size]
+
+stack = Stack()
+
+stack.push(2)
+stack.push(6)
+stack.push(10)
+
+stack.pop()
+
+print(stack)
