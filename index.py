41c7dcd2a9737521ef384464a42094e4b00f7557
henrynnamani <hoyx0101@gmail.com>
2026-04-18 13:17:08 +0100

    feat: model storage

--- index.py
+++ index.py
@@ -32,13 +32,56 @@ class SimpleMemory:
 
     def access(self, offset, size):
         return self.memory[offset:offset+size]
+    def load(self, offset):
+        return self.access(offset, 32)
+    def store(self, offset, value):
+        self.memory[offset:offset+len(value)] = value
 
-stack = Stack()
+class Memory(SimpleMemory):
+    def store(self, offset, value):
+        old_cost = self.calc_memory_expansion_gas()
+        if len(self.memory) <= offset + len(value):
+            expansion_size = 0
+            if len(self.memory) == 0:
+                expansion_size = 32
+                self.memory = [0x00 for _ in range(32)]
 
-stack.push(2)
-stack.push(6)
-stack.push(10)
+            if len(self.memory) < offset + len(value):
+                expansion_size += offset + len(value) - len(self.memory)
+                self.memory.extend([0x00] * expansion_size)
 
-stack.pop()
+        super().store(offset, value)
+        new_cost = self.calc_memory_expansion_gas()
+        return new_cost - old_cost
+    
+    def calc_memory_expansion_gas(self):
+        memory_size_word = (len(self.memory) + 31) / 32
+        memory_cost = (memory_size_word ** 2) / 512 + (3 * memory_size_word)
+        return round(memory_cost)
+    
+class KeyValue:
+    def __init__(self): 
+        self.storage = {}
 
-print(stack)
+    def load(self, key): 
+        return self.storage[key]
+    def store(self, key, value): self.storage[key] = value
+
+class Storage(KeyValue):
+    def __init__(self):
+        super().__init__()
+        self.cache = []
+
+    def load(self, key):
+        warm = True if key in self.cache else False
+        if not warm:
+            self.cache.append(key)
+        if key not in self.storage: return 0x00
+        return warm, super().load(key)
+
+storage = Storage()
+
+storage.store(1, 420)
+
+print(storage.load(1))
+print(storage.load(506))
